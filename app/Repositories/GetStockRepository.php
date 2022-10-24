1807d51e5efa5f8c0c10b35d8218a6e1d8ebafab
Colten <likeyou600@gmail.com>
2022-10-25 03:35:16 +0800

    Feat : 拿取種類最新資料時 分頁

--- app/Repositories/GetStockRepository.php
+++ app/Repositories/GetStockRepository.php
@@ -51,7 +51,8 @@ class GetStockRepository
 
     public function get_category_last_stock($request)
     {
-        $stocks = StockCategory::find($request->stock_category_id)->StockName;
+        $page = $request->page;
+        $stocks = StockCategory::find($request->stock_category_id)->StockName->slice($page * 10, $page + 10)->values();
         $stock_data = $stocks->map(function ($item) {
             return $item->StockData->last();
         });
@@ -66,7 +67,7 @@ class GetStockRepository
             return $item;
         });
         return response()->json([
-            'data' => $stock_data
+            'stocks' => $stock_data
         ], 200);
     }
 
